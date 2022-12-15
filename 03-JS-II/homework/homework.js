4e766173132690260deb83ad88db5f500d403100
erarregui <41311083+erarregui@users.noreply.github.com>
2022-12-15 08:33:41 -0300

    Update homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -63,7 +63,7 @@ function saludo(idioma) {
 }
 
 function colors(color) {
-  //La función recibe un color. Devolver el string correspondiente:
+  //La función recibe un color. Devolver el string correspondiente :
   //En caso que el color recibido sea "blue", devuleve --> "This is blue"
   //En caso que el color recibido sea "red", devuleve --> "This is red"
   //En caso que el color recibido sea "green", devuleve --> "This is green"
